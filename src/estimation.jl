8eb7e63ba71a39589210e2050396813614acc95a
GMizunogit config --global user.name GMizunogit config --global user.name GMizuno <gabrielmizuno@gmail.com>
2020-03-27 14:32:26 -0300

    Mais alterações

--- src/estimation.jl
+++ src/estimation.jl
@@ -1,15 +1,23 @@
 using LinearAlgebra, Optim, NLSolversBase
 
+function ensure_is_matrix(y::Vector{T}) where T
+    return y[:, :]
+end
+function ensure_is_matrix(y::Matrix{T}) where T
+    return y
+end
+
 struct Modelo{T} 
     Y::Vector{Int}
-    X::VecOrMat{T}
+    X::Matrix{T}
+
     function Modelo(Y::Vector{Int}, X::VecOrMat{T}) where {T<:Real}
+        X = ensure_is_matrix(X)
         return new{T}(Y, X) 
     end
 end
 
-
-struct estimations{T}
+struct Estimations{T}
     beta_hat::Vector{T}
     sigma::Matrix{T}
     pi_hat::Vector{Float64}
@@ -25,7 +33,7 @@ end
 
 function estimate(modelo::Modelo{T}) where {T<:Real}
     X = modelo.X
-     Y = modelo.Y
+    Y = modelo.Y
     numpar = length(size(X)) == 1 ? 1 : size(X)[2]
     numobs = length(Y)
 
@@ -43,11 +51,10 @@ function estimate(modelo::Modelo{T}) where {T<:Real}
      dem = ones(numobs) + exp.(X*beta_hat)
      πi = num./dem 
 
-    return estimations(beta_hat, var_beta, πi)
+    return Estimations(beta_hat, var_beta, πi)
 end
 
-function AIC(modelo::Modelo{T}) where {T<:Real}
-    X = modelo.X
+function aic(modelo::Modelo{T}) where {T<:Real}
     Y = modelo.Y
     beta_hat = estimate(modelo).beta_hat; LbetaML = loglike(modelo, beta_hat)
     p = length(beta_hat); n = length(Y)
@@ -55,7 +62,6 @@ function AIC(modelo::Modelo{T}) where {T<:Real}
 end
 
 function y_hat(modelo::Modelo{T}, threshold::Float64) where {T<:Real}
-    X = modelo.X
     Y = modelo.Y
     pi_hat = estimate(modelo).pi_hat
     y_hat = map(x-> x > threshold ? 1 : 0, pi_hat)
@@ -63,8 +69,9 @@ function y_hat(modelo::Modelo{T}, threshold::Float64) where {T<:Real}
 end
 
 function residuals_dev(modelo::Modelo{T}) where {T<:Real}
-    beta_hat = estimate(modelo).beta_hat
-    pihat = estimate(modelo).pi_hat
+    estimations = estimate(modelo)
+    beta_hat = estimations.beta_hat
+    pihat = estimations.pi_hat
     Y = modelo.Y
     numobs = length(Y)
     return sign.(Y-pihat).*sqrt.(-2*Y.*log.(pihat) -2*(ones(numobs) - Y).*log.(ones(numobs) - pihat))
