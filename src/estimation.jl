70566c3fe4188a6faf37c16accf0c2155f86d8b9
GMizunogit config --global user.name GMizunogit config --global user.name GMizuno <gabrielmizuno@gmail.com>
2020-04-02 17:18:22 -0300

    Mais alterações

--- src/estimation.jl
+++ src/estimation.jl
@@ -1,15 +1,15 @@
 # This function should find beta_hat
-function maxllk(y::Vector{Int}, X::VecOrMat{T}, numpar::Int) where {T<:Real}
-    func = TwiceDifferentiable(vars -> -loglik(y, X, vars[1:numpar], length(y)), ones(numpar); autodiff=:forward);
-    return optimize(func, ones(numpar))
+function maxllk(y::Vector{Int}, X::VecOrMat{T}, num_par::Int) where {T<:Real}
+    func = TwiceDifferentiable(vars -> -loglik(y, X, vars, length(y)), ones(num_par); autodiff=:forward);
+    return optimize(func, ones(num_par))
 end
 
 function fit(opt)
     return Optim.minimizer(opt)
 end
 
-function eval_pi_hat(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{T}, numobs) where {T<:Real}
-    return exp.(X*beta_hat)./( ones(numobs) + exp.(X*beta_hat) )
+function eval_pi_hat(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{T}, num_obs) where {T<:Real}
+    return exp.(X*beta_hat)./( ones(num_obs) + exp.(X*beta_hat) )
 end
 
 function eval_y_hat(pi_hat::Vector{Float64}, threshold::Float64)
@@ -18,14 +18,6 @@ end
 
 function loglik(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{S}, num_obs::Int) where {T<:Real, S} 
     return sum(y .* X * beta_hat - log.(ones(num_obs) + exp.(X * beta_hat)))
-end
-
-function eval_aic(llk::T, dof_log::Int) where {T<:Real}
-    return 2*log(llk) + 2*dof_log
-end
-
-function eval_bic(llk::T, dof_log::Int, y::Vector{Int}) where {T<:Real}
-    return 2*log(llk) + dof_log*log(y)
 end
 
 
@@ -39,12 +31,11 @@ end
 Performs logistic regression.
 """
 function logreg(y::Vector{Int}, X::Matrix{T}, threshold::Float64 = .5) where T 
-    num_obs, num_par = size(X) # esse num_par nao esta contando o beta0
-
+    num_obs, num_par = size(X)
     # Faz todas as funções necessárias
     maxllk    = maxllk(y, X, num_par)
     beta_hat  = fit(maxllk)
-    dof_log   = num_par + 1 # +1 é par contar o beta0
+    dof_log   = num_par
     dof_resid = num_obs - num_par
     dof_total = num_obs - 1
     pi_hat    = eval_pi_hat(y, X, beta_hat, num_obs)
