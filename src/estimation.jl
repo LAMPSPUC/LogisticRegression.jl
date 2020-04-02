a194079aab45ab967697b9790274e7679ed239a2
andregutierrez3 <andre_gutierrez3@hotmail.com>
2020-04-02 16:59:42 -0300

    fixing bugs

--- src/estimation.jl
+++ src/estimation.jl
@@ -27,15 +27,14 @@ function loglik(y::Vector{Int}, X::Matrix{T}, beta_hat::Vector{T}, num_obs::Int)
     return sum(y .* X * beta_hat - log.(ones(num_obs) + exp.(X * beta_hat)))
 end
 
-function eval_aic(llk::T, dof_log::Int) where T
-    return -2/length(Model.y) * llk + 2 * dof_log / length(Model.y) #for that method the model with the lowest AIC is the best
-end
+function eval_aic(llk::T, dof_log::Int, Model{T <: Real}) where T
+    return -2/length(Model.y) * llk + 2 * dof_log / length(Model.y) 
 
-function eval_bic(llk::T, dof_log::Number) where T
+function eval_bic(llk::T, dof_log::Int, Model{T <: Real}) where T
     return length(Model.y) * llk + dof_log * log(length(Model.y))
 end
 
-function eval_r2(LL1::Number, LL0::Number) where T
+function eval_r2(LL1::T, LL0::T) where T
     return 1-LL1/LL0
 end
 
@@ -44,7 +43,7 @@ function logreg(y::Vector{Number}, X::Vector{T}) where T
     return logreg(y, X[:, :])
 end
 
-function sigma(y, X, beta_hat, num_obs)
+function sigma(y::Vector{Int}, X::Matrix{T}, beta_hat::Vector{T}, num_obs::Int) where T
     
     numpar = length(size(X)) == 1 ? 1 : size(X)[2]
     func = TwiceDifferentiable(vars -> -loglik(y, X, beta_hat, num_obs), ones(numpar+1); autodiff=:forward)
@@ -52,17 +51,19 @@ function sigma(y, X, beta_hat, num_obs)
     
 end
 
-function deviance_residuals(y::Vector{Number}, prevision::Vector{Number})
+function deviance_residuals(y::Vector{Int}, prevision::Vector{T}) where T
     dev =  sign(y.-prevision).*(-2.*(y.*log(prevision)+(1.-y).*log(1.-prevision)))^(1/2)
     return dev
 
-function deviance_residuals_variance(dev::Vector{Number})
-    for resi in dev:
+function deviance_residuals_variance(dev::Vector{T}) where T
+    for resi in dev
         x = (resi - mean(dev))^(2)
+    end
     return x/(lenght(dev)-1)
+end
 
 function std_error(sigma::Matrix)
-    return (sigma.^(1/2)
+    return sigma.^(1/2)
 end
 
 """
