a37022d6d1949d8af37c2acfc372d525ae79f2ce
GMizunogit config --global user.name GMizunogit config --global user.name GMizuno <gabrielmizuno@gmail.com>
2020-04-02 16:56:38 -0300

    Fazendo alterações

--- src/estimation.jl
+++ src/estimation.jl
@@ -1,44 +1,35 @@
 # This function should find beta_hat
-function fit(y, X, numpar)
+function maxllk(y::Vector{Int}, X::VecOrMat{T}, numpar::Int) where {T<:Real}
     func = TwiceDifferentiable(vars -> -loglik(y, X, vars[1:numpar], length(y)), ones(numpar); autodiff=:forward);
-    opt  = optimize(func, ones(numpar))
+    return optimize(func, ones(numpar))
+end
+
+function fit(opt)
     return Optim.minimizer(opt)
 end
 
-function eval_dof_log(beta_hat)
-    return length(beta_hat)
+function eval_pi_hat(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{T}, numobs) where {T<:Real}
+    return exp.(X*beta_hat)./( ones(numobs) + exp.(X*beta_hat) )
 end
 
-function eval_dof_resid(y, beta_hat)
-    return length(y) - length(beta_hat)
-end
-
-function eval_dof_total(y, beta_hat)
-    return length(y) - 1
-end
-
-function eval_pi_hat(y, X, beta_hat, numobs)
-    exp.(X*beta_hat)./( ones(numobs) + exp.(X*beta_hat) )
-end
-
-function eval_y_hat(pi_hat, threshold)
+function eval_y_hat(pi_hat::Vector{Float64}, threshold::Float64)
     map(x-> x > threshold ? 1 : 0,  pi_hat)
 end
 
 function loglik(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{S}, num_obs::Int) where {T<:Real, S} 
-    return sum(y .* hcat(ones(num_obs), X) * beta_hat - log.(ones(num_obs) + exp.(hcat(ones(num_obs), X) * beta_hat)))
+    return sum(y .* X * beta_hat - log.(ones(num_obs) + exp.(X * beta_hat)))
 end
 
-function eval_aic(llk::T, dof_log::Int) where T
+function eval_aic(llk::T, dof_log::Int) where {T<:Real}
     return 2*log(llk) + 2*dof_log
 end
 
-function eval_bic(llk::T, dof_log::Int, y::Vector{Int}) where T
+function eval_bic(llk::T, dof_log::Int, y::Vector{Int}) where {T<:Real}
     return 2*log(llk) + dof_log*log(y)
 end
 
 
-function logreg(y::Vector{Int}, X::Vector{T}) where T
+function logreg(y::Vector{Int}, X::Vector{T}) where {T<:Real}
     return logreg(y, X[:, :])
 end
 
@@ -48,15 +39,15 @@ end
 Performs logistic regression.
 """
 function logreg(y::Vector{Int}, X::Matrix{T}, threshold::Float64 = .5) where T 
-    numobs, numpar = size(X)
+    num_obs, num_par = size(X) # esse num_par nao esta contando o beta0
 
     # Faz todas as funções necessárias
-    beta_hat  = fit(y, X)
-    num_obs   = length(y)
-    dof_log   = eval_dof_log(y)
-    dof_resid = eval_dof_resid(y, beta_hat)
-    dof_total = eval_dof_total(y, beta_hat)
-    pi_hat    = eval_pi_hat(y, X, beta_hat, numobs)
+    maxllk    = maxllk(y, X, num_par)
+    beta_hat  = fit(maxllk)
+    dof_log   = num_par + 1 # +1 é par contar o beta0
+    dof_resid = num_obs - num_par
+    dof_total = num_obs - 1
+    pi_hat    = eval_pi_hat(y, X, beta_hat, num_obs)
     y_hat     = eval_y_hat(pi_hat, threshold)
     llk       = loglik(y, X, beta_hat)
     aic       = eval_aic(llk, dof_log)
