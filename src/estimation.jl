074df34838120a7e8f43ac0e51e692b51fb88de7
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2020-04-03 00:31:29 -0300

    update functions

--- src/estimation.jl
+++ src/estimation.jl
@@ -1,25 +1,28 @@
 # This function should find beta_hat
-function maxllk(y::Vector{Int}, X::VecOrMat{T}, num_par::Int) where {T<:Real}
-    func = TwiceDifferentiable(vars -> -loglik(y, X, vars, length(y)), ones(num_par); autodiff=:forward);
+function maxllk(y::Vector{Int}, X::Matrix{T}, num_par::Int, num_obs::Int) where T <: Real
+    func = TwiceDifferentiable(vars -> -optim_loglik(y, X, vars, num_obs), ones(num_par); autodiff=:forward)
     return optimize(func, ones(num_par))
 end
 
-function fit(opt)
+function eval_beta_hat(opt)
     return Optim.minimizer(opt)
 end
 
-function eval_pi_hat(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{T}, num_obs) where {T<:Real}
-    return exp.(X*beta_hat)./( ones(num_obs) + exp.(X*beta_hat) )
+function eval_pi_hat(y::Vector{Int}, X::Matrix{T}, beta_hat::Vector{T}, num_obs::Int) where T <: Real
+    return exp.(X*beta_hat)./(ones(num_obs) + exp.(X*beta_hat))
 end
 
-function eval_y_hat(pi_hat::Vector{Float64}, threshold::Float64)
-    map(x-> x > threshold ? 1 : 0,  pi_hat)
+function eval_y_hat(pi_hat::Vector{T}, threshold::T) where T <: Real
+    return map(x -> x > threshold ? 1 : 0, pi_hat)
 end
 
-function loglik(y::Vector{Int}, X::VecOrMat{T}, beta_hat::Vector{S}, num_obs::Int) where {T<:Real, S} 
+function optim_loglik(y::Vector{Int}, X::Matrix{T}, beta_hat::Vector{T}, num_obs::Int) where T <: Real 
     return sum(y .* X * beta_hat - log.(ones(num_obs) + exp.(X * beta_hat)))
 end
 
+function eval_loglik(opt)
+    return Optim.minimum(opt)
+end
 
 function logreg(y::Vector{Int}, X::Vector{T}) where {T<:Real}
     return logreg(y, X[:, :])
@@ -30,11 +33,11 @@ end
 
 Performs logistic regression.
 """
-function logreg(y::Vector{Int}, X::Matrix{T}, threshold::Float64 = .5) where T 
+function logreg(y::Vector{Int}, X::Matrix{T}, threshold::Float64 = 0.5) where T 
     num_obs, num_par = size(X)
     # Faz todas as funções necessárias
     maxllk    = maxllk(y, X, num_par)
-    beta_hat  = fit(maxllk)
+    beta_hat  = eval_beta_hat(maxllk)
     dof_log   = num_par
     dof_resid = num_obs - num_par
     dof_total = num_obs - 1
@@ -42,6 +45,6 @@ function logreg(y::Vector{Int}, X::Matrix{T}, threshold::Float64 = .5) where T
     y_hat     = eval_y_hat(pi_hat, threshold)
     llk       = loglik(y, X, beta_hat)
     aic       = eval_aic(llk, dof_log)
-    bic       = eval_bic(llk::T, dof_log::Int, y)
+    bic       = eval_bic(llk, dof_log, y)
     return Model(y, X, ...)
 end
